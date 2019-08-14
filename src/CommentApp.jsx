f2133251e59b58bff2addf2d9ef6edb5ce209817
w6a <583783099@qq.com>
2019-08-14 16:12:50 +0800

    评论持久化

--- src/CommentApp.jsx
+++ src/CommentApp.jsx
@@ -11,14 +11,34 @@ class CommentApp extends React.Component {
     this.handleSubmitComment = this.handleSubmitComment.bind(this);
   }
 
+  componentDidMount() {
+    this._loadComments();
+  }
+
+  _loadComments() {
+    let comments = localStorage.getItem('comments');
+    if (comments) {
+      comments = JSON.parse(comments);
+      this.setState({
+        comments
+      });
+    }
+  }
+
+  _saveComments(comments) {
+    localStorage.setItem('comments', JSON.stringify(comments));
+  }
+
   handleSubmitComment(comment) {
     if (!comment) return;
     if (!comment.username) return alert('请输入用户名');
     if (!comment.content) return alert('请输入评论内容');
     this.state.comments.push(comment);
+    const comments = this.state.comments;
     this.setState({
-      comments: this.state.comments
+      comments
     });
+    this._saveComments(comments);
   }
 
   render() {
