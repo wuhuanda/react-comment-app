736393d26826cd7866161d9fa34d9dfb6a1bc459
w6a <583783099@qq.com>
2019-08-14 10:49:16 +0800

    增加提示

--- src/CommentApp.jsx
+++ src/CommentApp.jsx
@@ -12,6 +12,9 @@ class CommentApp extends React.Component {
   }
 
   handleSubmitComment(comment) {
+    if (!comment) return;
+    if (!comment.username) return alert('请输入用户名');
+    if (!comment.content) return alert('请输入评论内容');
     this.state.comments.push(comment);
     this.setState({
       comments: this.state.comments
